8b19db3c3f7e83890d141324bf22bf18e1f4783f
MariamElkhouly <152817864+MariamElkhouly@users.noreply.github.com>
2024-01-06 15:07:37 +0200

    .

--- grid.h
+++ grid.h
@@ -26,10 +26,5 @@ public:
 	void loadGame(const string& filename);// Load the game state from a text file
 
 
-	brick* getBrick(int row, int column);
-	void removeBrick(point Clicked);
-	void saveGame(const string& filename) const; // Save the game state to a text file
-	void loadGame(const string& filename);// Load the game state from a text file
-
 };
 
